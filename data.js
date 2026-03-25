5e75e3abbbf30da47032e7eb20f932063371fde8
dafsgzassdasda-png <dafsgzassdasda@gmail.com>
2026-03-25 18:14:21 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-360-banheiro-feminino-igreja-so-franciscojpg",
+      "name": "360 banheiro feminino igreja são francisco.jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9255185800978118,
+          "pitch": 0.6405519346595341,
+          "rotation": 0,
+          "target": "1-360-banheiro-masculino-igreja-so-franciscojpg"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-360-banheiro-masculino-igreja-so-franciscojpg",
+      "name": "360 banheiro masculino igreja são francisco.jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8907704404876,
+          "pitch": 0.4923290452119513,
+          "rotation": 0,
+          "target": "0-360-banheiro-feminino-igreja-so-franciscojpg"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
